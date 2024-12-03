799ce6bddb6da78d29d3ab1824d14767a01ed35f
categoraal <76538493+categoraal@users.noreply.github.com>
2024-12-03 16:22:32 +0100

    made day 3 faster

--- day03.py
+++ day03.py
@@ -1,52 +1,32 @@
 data = open('3').read().strip()
 
-res= []
+p1 = p2 = 0
+enable = True
 for i in range(len(data)):
-    string = data
-    if string[i:].startswith('mul('):
-        f1 = True;f2 = True;f3 = False
+    if data[i:].startswith("do()"):
+        enable = True
+    if data[i:].startswith("don't()"):
+        enable = False
+    if data[i:].startswith('mul('):
+        f1 = f2=True;f3 = False
         i += 4
-        c1,c2 = '',''
+        c1=c2 = ''
         while f1: 
-            if string[i].isnumeric() and f2:
-                c1 += string[i]
-            elif string[i] == ',' and f2:
+            if data[i].isnumeric() and f2:
+                c1 += data[i]
+            elif data[i] == ',' and f2:
                 f3 = True
                 f2 = False
-            elif string[i].isnumeric() and f3:
-                c2 += string[i]   
-            elif string[i] == ')' and f3:
-                res.append(int(c1)*int(c2))
-                f1=f2=f3=False
+            elif data[i].isnumeric() and f3:
+                c2 += data[i]   
+            elif data[i] == ')' and f3:
+                p1 += int(c1)*int(c2)
+                if enable:
+                    p2 += int(c1)*int(c2)
+                f1 = False
             else:
                 break
             i += 1
-print(sum(res))
 
-res = []
-enable = True
-dd = data.split('do()')
-for k in dd:
-    k = k.split("don't()")
-    for i in range(len(k[0])):
-        string = k[0]
-        if string[i:].startswith('mul(') and enable:
-            f1 = True;f2 = True;f3 = False
-            i += 4
-            c1,c2 = '',''
-            while f1: 
-                if string[i].isnumeric() and f2:
-                    c1 += string[i]
-                elif string[i] == ',' and f2:
-                    f3 = True
-                    f2 = False
-                elif string[i].isnumeric() and f3:
-                    c2 += string[i]   
-                elif string[i] == ')' and f3:
-                    res.append(int(c1)*int(c2))
-                    f1=f2=f3=False
-                else:
-                    break
-                i += 1
-
-print(sum(res))
+print(p1)
+print(p2)
