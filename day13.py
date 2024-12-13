967efe92c1b4f6c88373ed5b746c983a86a45658
categoraal <76538493+categoraal@users.noreply.github.com>
2024-12-13 08:09:40 +0100

    day 13 (no regex or sympy)

--- day13.py
+++ day13.py
@@ -1,12 +1,10 @@
-import re
-from decimal import Decimal
 data = open('13').read().strip().split('\n\n')
 
 t = 10000000000000
 p1 = p2 = 0
 for i,v in enumerate(data):
-    n = [int(i) for i in re.findall(r'\d+',v)]
-    x1,y1,x2,y2,x,y = n
+    n = [x for xss in [[int(''.join([f for f in j if f.isnumeric()])) for j in k.split(',')] for k in v.split('\n')] for x in xss]
+    x1,y1,x2,y2,x,y = n 
     for i in range(100):
         for j in range(100):
             if x1*i+x2*j == x and y1*i+y2*j == y:
@@ -14,22 +12,12 @@ for i,v in enumerate(data):
     
     x += t
     y += t
-    d = Decimal(x1*y2-x2*y1)
-    i = Decimal((y2*x-x2*y)*(1/d))
-    j = Decimal((-y1*x+x1*y)*(1/d))
-    if i%1 == 0.0 and j%1 == 0.0:
-        if int(i)*x1+int(j)*x2 == x:
-            p2 += 3*i+j
-    elif i%1 < 0.999:
-        i = int(str(i).split('.')[0])
-        j = int(str(j).split('.')[0])
-        if i*x1+j*x2 == x:
-            p2 += 3*i+j
-    elif i%1 > 0.001:
-        i = int(str(i).split('.')[0])+1
-        j = int(str(j).split('.')[0])+1
-        if i*x1+j*x2 == x:
-            p2 += 3*i+j
+    d = (x1*y2-x2*y1)
+    i = ((y2*x-x2*y)/d)
+    j = ((-y1*x+x1*y)/d)
+    for n in [-1,0,1]:
+        if (int(i)+n)*x1+(int(j)+n)*x2 == x and (int(i)+n)*y1+(int(j)+n)*y2 == y:
+            p2 += (int(i)+n)*3+(int(j)+n)
 
 print(p1)
-print(int(p2))
+print(p2)
