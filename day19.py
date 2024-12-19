5c624da6fbcbd298685bb9f6b97bf35218d08c54
categoraal <76538493+categoraal@users.noreply.github.com>
2024-12-19 07:47:06 +0100

    day 19 compact

--- day19.py
+++ day19.py
@@ -6,21 +6,16 @@ designs = designs.split('\n')
 cache = {}
 def solve(x):
     r = 0
-    if x in cache:
-        return cache[x]    
+    if x in cache: return cache[x]    
     for c in colors:
-        if c == x:
-            r += 1
-        if x.startswith(c):
-            l = len(c)
-            r += solve(x[l:])
+        if c == x: r += 1
+        if x.startswith(c): r += solve(x[len(c):])
     cache[x] = r
     return r
-p1 = p2= 0
 
+p1 = p2= 0
 for design in designs:
     n=solve(design)
     p2 += n
     p1 += 1 if n > 0 else 0
-print(p1)
-print(p2)
+print(p1,p2,sep='\n')
